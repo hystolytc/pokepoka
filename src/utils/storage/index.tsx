85442f532fabf4f0804c7b85020fb168215f40b2
hystolytc <melife4@gmail.com>
2022-01-30 00:04:48 +0700

    feat: create utility related pokemon list page
    create storage and context hooks utility

---
+++ src/utils/storage/index.tsx
@@ -0,0 +1,13 @@
+import { IMyPokemon } from 'types/myPokemon'
+
+export const getLocalStorage = {
+  myPokemon: (): IMyPokemon => {
+    let data = localStorage.getItem('my_pokemon')
+    if (data) return JSON.parse(data) 
+    return { pokemons: [], pokemonTotal: {} } as IMyPokemon
+  }
+}
+
+export const setLocalStorage = {
+  myPokemon: (data: IMyPokemon) => localStorage.setItem('my_pokemon', JSON.stringify(data)) 
+}
