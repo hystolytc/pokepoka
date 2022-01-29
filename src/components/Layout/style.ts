a5c2773be98237be37e2365285ad308b0e81b9ba
hystolytc <melife4@gmail.com>
2022-01-30 00:01:35 +0700

    feat: create pokemon list page
    create container and component for pokemon list page

---
+++ src/components/Layout/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+import { color } from 'styles'
+
+const Layout = styled.div`
+  position: relative;
+  max-width: 480px;
+  min-height: calc(100vh - 5opx);
+  margin-left: auto;
+  margin-right: auto;
+  background-color: ${color.white};
+`
+
+export { Layout }
