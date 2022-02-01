10f083e473a1d7a8771e13dd81587b85c90122ad
hystolytc <melife4@gmail.com>
2022-02-01 18:49:49 +0700

    style: add new style for pokemon detail page
    register new color

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -4,7 +4,7 @@ import { color } from 'styles'
 const Layout = styled.div`
   position: relative;
   max-width: 480px;
-  min-height: calc(100vh - 5opx);
+  min-height: calc(100vh - 50px);
   margin-left: auto;
   margin-right: auto;
   background-color: ${color.white};
--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,4 +1,10 @@
 export const color = {
   white: '#fff',
-  red400: 'rgba(248, 113, 113)'
+  red400: 'rgb(248, 113, 113)',
+  red600: 'rgba(220, 38, 38)',
+  grey100: 'rgb(243 244 246)',
+  grey400: 'rgba(156, 163, 175)',
+  textPrimary: '#4a4a4a',
+  black70: 'rgba(0,0,0,0.7)',
+  yellow300: 'rgba(252, 211, 77)'
 }
