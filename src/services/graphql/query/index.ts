cd85ac08f2202e895428b0906241b48ee85a62fe
hystolytc <melife4@gmail.com>
2022-02-01 18:50:31 +0700

    feat: add pokemon detail graphql service
    add GET_POKEMON_DETAIL for query pokemon detail

--- src/services/graphql/query/index.ts
+++ src/services/graphql/query/index.ts
@@ -13,3 +13,32 @@ export const GET_POKEMON = gql`
     }
   }
 `
+
+export const GET_POKEMON_DETAIL = gql`
+  query getPokemonDetail($name: String) {
+    pokemon: pokemon_v2_pokemon(where: {name: {_eq: $name}}) {
+      id
+      name
+      types: pokemon_v2_pokemontypes {
+        type: pokemon_v2_type {
+          name
+          moves: pokemon_v2_moves(order_by: {name: asc}) {
+            name
+            accuracy
+          }
+        }
+      }
+      stats: pokemon_v2_pokemonstats {
+        stat: pokemon_v2_stat {
+          name
+        }
+        base_stat
+      }
+      abilities: pokemon_v2_pokemonabilities {
+        ability: pokemon_v2_ability {
+          name
+        }
+      }
+    }
+  }
+`
